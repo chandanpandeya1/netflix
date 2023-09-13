97b079fda543c70d5cdfd1165ae0158b38026861
chandanpandeya1 <chandanpandeya1@gmail.com>
2023-09-13 12:43:32 +0530

    first commit

---
+++ src/Sdata.jsx
@@ -0,0 +1,37 @@
+const Sdata = [
+    {
+        imgsrc: "https://img10.hotstar.com/image/upload/f_auto/sources/r1/cms/prod/6912/1366912-i-0714e3df944a",
+        title: "A Netflix orignal Series",
+        sname: "DARK",
+        link: "https://www.hotstar.com/in/movies/x-men-dark-phoenix/1260017860",
+    },
+    {
+        imgsrc: "https://img10.hotstar.com/image/upload/f_auto/sources/r1/cms/prod/5693/1405693-i-d32e11fe5fd9",
+        title: "A Netflix orignal Series",
+        sname: "Brahmastra",
+        link: "https://www.hotstar.com/in/movies/brahmastra-part-one-shiva/1260110227",
+    },
+
+
+    {
+        imgsrc: "https://img10.hotstar.com/image/upload/f_auto/sources/r1/cms/prod/8698/1418698-i-ea1d9b523dc4",
+        title: "A Netflix orignal Series",
+        sname: "The Loin King",
+        link: "https://www.hotstar.com/in/movies/the-lion-king/1260014782",
+    },
+
+    {
+        imgsrc: "https://img10.hotstar.com/image/upload/f_auto/sources/r1/cms/prod/48/1370048-i-7fd17e1f03d6",
+        title: "A Netflix orignal Series",
+        sname: "Avengers",
+        link: "https://www.hotstar.com/in/movies/marvels-the-avengers/1660000015",
+    },
+
+    {
+        imgsrc: "https://img10.hotstar.com/image/upload/f_auto/sources/r1/cms/prod/219/1530219-i-898c101fb615",
+        title: "A Netflix orignal Series",
+        sname: "Avatar",
+        link: "https://www.hotstar.com/in/movies/avatar-the-way-of-water/1260142021",
+    },
+]
+export default Sdata;
